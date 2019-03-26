e02d9e3051e4731e7b81f1ff3668f80a235ed8a4
Gary Figuerola <garyfimo@gmail.com>
2019-03-26 15:26:39 -0500

    firt commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+n
+>app/src/main/java/belcorp/biz/customsearchview/MainActivity.kt,8/a/8a6d397d3bce845d28bdadc160ade1a1a9fc4ebc
+l
+<app/src/main/java/belcorp/biz/customsearchview/SearchView.kt,f/c/fc5efbbc443ff97347ae8a3cb484188144bda841
---
+++ app/src/main/java/belcorp/biz/customsearchview/MainActivity.kt
@@ -0,0 +1,12 @@
+package belcorp.biz.customsearchview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
